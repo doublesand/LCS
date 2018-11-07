03e58089654086d92fd17d03477bce1d0aecd929
doublesand <doublesand@163.com>
2018-11-07 22:42:11 +0800

    小修改

--- LCS/main.cpp
+++ LCS/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+const int M = 100;  //第一个字符串长度
+const int N = 100;  //第二个字符串长度
+
+int lcs_length(char x[], char y[], int b[][N]) {
+	
+}
 
 int main() {
 	cout << "LCS算法";
